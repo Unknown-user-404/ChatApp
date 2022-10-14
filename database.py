31def2f2c173a973d93eb636ef4b4f97f72749a4
Unknown-user-404 <ffje53246@gmail.com>
2022-10-13 22:40:34 -0400

    Added files

---
+++ database.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+from werkzeug.security import generate_password_hash
+from dotenv import load_dotenv
+import os
+load_dotenv()
+mongo_password = os.getenv('mongo_password')
+
+client = MongoClient(f'mongodb+srv://admin:{mongo_password}@cluster0.y8pvuwe.mongodb.net/?retryWrites=true&w=majority')
+chat_db = client.get_database("ChatDB")
+users_collection = chat_db.get_collection('users')
+
+def save_user(username, email, password):
+    password_hash = generate_password_hash(password)
+    users_collection.insert_one({'_id': username, 'email': email, 'password': password_hash})
+
+save_user("nikhil", "a@a.com", "test")
