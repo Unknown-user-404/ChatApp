508082f881e73dfb70010d83867946867c040eae
Unknown-user-404 <ffje53246@gmail.com>
2022-10-14 17:32:34 -0400

    Updated
    Updated version

--- app.py
+++ app.py
@@ -1,17 +1,68 @@
-from flask_login import LoginManager
+import os
+from flask_login import LoginManager, login_required, login_user, logout_user, current_user
 from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO, join_room
+from pymongo.errors import DuplicateKeyError
+from dotenv import load_dotenv
+load_dotenv()
+
+from database import get_user, save_user
 
 app = Flask(__name__)
+app.secret_key = os.getenv('secret_key')
 socketio = SocketIO(app)
 login_manager = LoginManager()
+login_manager.login_view = 'login'
 login_manager.init_app(app)
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    message = ''
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password_input = request.form.get('password')
+        user = get_user(username)
+
+        if user and user.check_password(password_input):
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            message = 'Failed to login: username or password incorrect'
+        
+    return render_template('login.html', message=message)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    message = ''
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        try:
+            save_user(username, email, password)
+            return redirect(url_for('login'))
+        except DuplicateKeyError:
+            message = 'User already exists!'
+    return render_template('signup.html', message=message)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
 @app.route('/chat')
+@login_required
 def chat():
     username = request.args.get('username')
     room = request.args.get('room')
@@ -31,6 +82,10 @@ def handle_join_room_event(data):
     app.logger.info(f"{data['username']} has joined room {data['room']}")
     join_room(data['room'])
     socketio.emit('join_room_announcement', data)
+
+@login_manager.user_loader
+def load_user(username):
+    return get_user(username)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
--- database.py
+++ database.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 from werkzeug.security import generate_password_hash
 from dotenv import load_dotenv
 import os
+from user import User
 load_dotenv()
 mongo_password = os.getenv('mongo_password')
 
@@ -13,4 +14,6 @@ def save_user(username, email, password):
     password_hash = generate_password_hash(password)
     users_collection.insert_one({'_id': username, 'email': email, 'password': password_hash})
 
-save_user("nikhil", "a@a.com", "test")
+def get_user(username):
+    user_data = users_collection.find_one({'_id': username})
+    return User(user_data['_id'], user_data['email'], user_data['password']) if user_data else None
